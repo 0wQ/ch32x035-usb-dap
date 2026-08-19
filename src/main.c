c34c0997f2a380475674bbe5e572d9e217e60a0a
Mizore <xvz@live.com>
2026-08-19 21:22:40 +0800

    feat: add target control drivers

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include "bsp/bsp_delay.h"
+#include "drv/drv_dp_pullup.h"
+#include "drv/drv_power_switch.h"
+#include "drv/drv_uart_mux.h"
 #include "wchlink/wchlink_usb.h"
 
 #include <system_ch32x035.h>
 
 int main(void) {
     SystemInit();
+    drv_dp_pullup_init();
+    drv_power_switch_init();
+    drv_uart_mux_init();
     bsp_delay_init();
+    bsp_delay_ms(10u);
+    drv_power_switch_set_enabled(true);
     wchlink_usb_init();
     for (;;) {
         wchlink_usb_process();
--- xmake.lua
+++ xmake.lua
@@ -19,7 +19,7 @@ target("firmware")
     set_toolchains("wch-riscv")
     set_warnings("all")
 
-    add_files("src/main.c", "src/bsp/bsp_delay.c", "src/usb/bsp_usb.c", "src/wchlink/*.c", "sdk/Startup/startup_ch32x035_highcode.S")
+    add_files("src/main.c", "src/bsp/bsp_delay.c", "src/drv/*.c", "src/usb/bsp_usb.c", "src/wchlink/*.c", "sdk/Startup/startup_ch32x035_highcode.S")
     add_files("sdk/Core/*.c", "sdk/Peripheral/src/*.c", "sdk/System/*.c")
     add_files("third_party/cherryusb/core/usbd_core.c", "third_party/cherryusb_port/usb_ch32x035_dc_usbfs.c")
     add_files("third_party/cherryrb/chry_ringbuffer.c")
